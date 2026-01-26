275861b9b4239ed35376ea85124ad90dbbeb11d5
imagalhaess <magalhaes569@gmail.com>
2026-01-26 20:38:37 -0300

    refactor/ms: finaliza ultimas correcoes apos refatoramento.

--- ms-user/src/test/java/nttdata/personal/julius/api/infrastructure/persistence/repository/UserJpaRepositoryTest.java
+++ ms-user/src/test/java/nttdata/personal/julius/api/infrastructure/persistence/repository/UserJpaRepositoryTest.java
@@ -4,7 +4,7 @@ import nttdata.personal.julius.api.infrastructure.persistence.entity.UserEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
